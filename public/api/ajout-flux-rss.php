555ccc6263c858da877546918d0e0ceb46deef4b
Gatien <contact@gatiendev.fr>
2024-06-17 15:42:29 +0200

    now fectch content of flux directly when adding a new flux

--- public/api/ajout-flux-rss.php
+++ public/api/ajout-flux-rss.php
@@ -5,6 +5,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/lib/tools.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/src/model/CategorieModel.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/src/model/EspaceModel.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/src/model/FluxModel.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/src/model/ArticleModel.php";
 
 session_start();
 
@@ -32,24 +33,23 @@ $url = $_POST["adresse"];
 
 // Cas où on souhaite créer un dossier (logique à prendre à part car on peut avoir un `id_espace` au lieu d'un `id_categorie`)
 if ($type_flux == "categorie") {
-    if(!isset($_POST["espace"])){
+    if (!isset($_POST["espace"])) {
         http_response_code(400);
         die(json_encode(["error" => "missing `espace` parameter"]));
     }
 
     $id_espace = $_POST["espace"];
 
-    if(isset($_POST["categorie"])){
+    if (isset($_POST["categorie"])) {
         $id_categorie = $_POST["categorie"];
-        if(!CategorieModel::appartientA($id_utilisateur, $id_categorie)){
+        if (!CategorieModel::appartientA($id_utilisateur, $id_categorie)) {
             http_response_code(401);
             die(json_encode(["error" => "this category does not belong to you"]));
         }
         CategorieModel::pushNewCategorieToDB($url, $id_categorie, $id_espace);
-    }
-    else{
+    } else {
         $id_espace = $_POST["espace"];
-        if(!EspaceModel::appartientA($id_utilisateur, $id_espace)){
+        if (!EspaceModel::appartientA($id_utilisateur, $id_espace)) {
             http_response_code(401);
             die(json_encode(["error" => "this espace does not belong to you"]));
         }
@@ -57,13 +57,14 @@ if ($type_flux == "categorie") {
     }
 
     header("Location: /");
-
 }
 
 if (!isset($_POST["categorie"])) {
     http_response_code(400);
     die(json_encode(["error" => "categorie parameter needed"]));
 }
+
+$id_flux = null;
 
 
 $id_utilisateur = $_SESSION["id_utilisateur"];
@@ -109,40 +110,49 @@ if ($type_flux == "yt") {
 }
 
 // Cas où le flux rss est un sujet google news
-else if($type_flux == "google-news"){
-    $url = "https://news.google.com/rss/search?hl=fr&gl=FR&ceid=FR%3Afr&oc=11?gl=FR&ceid=FR%253Afr&hl=fr&q=".urlencode($url);
+else if ($type_flux == "google-news") {
+    $url = "https://news.google.com/rss/search?hl=fr&gl=FR&ceid=FR%3Afr&oc=11?gl=FR&ceid=FR%253Afr&hl=fr&q=" . urlencode($url);
     $id_flux = null;
-    if(FluxModel::isFluxRSSindb($url)){
+    if (FluxModel::isFluxRSSindb($url)) {
         $id_flux = FluxModel::getIDFromURL($url);
-    }
-    else{
+    } else {
         $id_flux = FluxModel::ajouterFluxRSSindb($url, $type_flux);
     }
     CategorieModel::addRSSFluxToCategorie($id_flux, $id_categorie);
 }
 
 // Cas où le flux rss est un sujet bing news
-else if($type_flux == "bing-news"){
-    $url = "https://www.bing.com/news/search?q=".urlencode($url)."%20loc%3aFR&qft=sortbydate%3d%221%22&format=RSS";
+else if ($type_flux == "bing-news") {
+    $url = "https://www.bing.com/news/search?q=" . urlencode($url) . "%20loc%3aFR&qft=sortbydate%3d%221%22&format=RSS";
     $id_flux = null;
-    if(FluxModel::isFluxRSSindb($url)){
+    if (FluxModel::isFluxRSSindb($url)) {
         $id_flux = FluxModel::getIDFromURL($url);
-    }
-    else{
+    } else {
         $id_flux = FluxModel::ajouterFluxRSSindb($url, $type_flux);
     }
     CategorieModel::addRSSFluxToCategorie($id_flux, $id_categorie);
 }
 
-// Cas où le flux RSS est un flux RSS natif
-else if ($type_flux == "rss") {
-    $id_flux = FluxModel::ajouterFluxRSSindb($url, $type_flux);
-    CategorieModel::addRSSFluxToCategorie($id_flux, $id_categorie);
+if($id_flux == null) {
+    header("Location: ../");
+    exit;
 }
 
-else {
-    http_response_code(400);
-    die(json_encode(["error" => "type_flux parameter incorrect"]));
+$articles = getArticlesFromRSSFlux($id_flux, $url);
+// On récupère le dernier article en date du flux rss courant stocké dans la db
+$dernier_article = FluxModel::getDernierUrlArticle($id_flux);
+$dernier_url_en_date = "";
+// On procéde ensuite par comparaison via l'url de l'article.
+// On considère que si 2 articles ont le même url d'article alors ils sont identiques.
+if (!is_null($dernier_article)) {
+    $dernier_url_en_date = $dernier_article->getUrlArticle();
+}
+
+foreach ($articles as $article) {
+    if ($article->getUrlArticle() == $dernier_url_en_date) {
+        break;
+    }
+    ArticleModel::insertArticleIntoDB($article, $id_flux);
 }
 
 header("Location: ../index.php");
